c3342a8fa9a7456b1e40287ac23c540f4a24ffb8
The-Hoster <43914540+The-Hoster@users.noreply.github.com>
2018-10-06 21:29:14 +0200

    Update js.js

--- scripts/js.js
+++ scripts/js.js
@@ -2,40 +2,40 @@ window.onload = greeting;
 
 function greeting() {
 
-function checkTime(i) {
-  if (i < 10) {
-    i = "0" + i;
-  }
-  return i;
-}
+    function checkTime(i) {
+        if (i < 10) {
+            i = "0" + i;
+        }
+        return i;
+    }
 
-function time() {
-  var today = new Date();
-  var h = today.getHours();
-  var m = today.getMinutes();
-  var s = today.getSeconds();
-  // add a zero in front of numbers<10
-  m = checkTime(m);
-  s = checkTime(s);
-  document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
-  t = setTimeout(function() {
-    time()
-  }, 250);
-}
-time();
+    function time() {
+        var today = new Date();
+        var h = today.getHours();
+        var m = today.getMinutes();
+        var s = today.getSeconds();
+        // add a zero in front of numbers<10
+        m = checkTime(m);
+        s = checkTime(s);
+        document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+        t = setTimeout(function() {
+            time()
+        }, 250);
+    }
+    time();
 
-var dateJS = new Date();
-var hrs = dateJS.getHours();
+    var dateJS = new Date();
+    var hrs = dateJS.getHours();
 
-	var greeting;
+    var greeting;
 
-	if (hrs < 12)
-	        greeting = 'Good Morning!';
-	    else if (hrs >= 12 && hrs <= 17)
-	        greeting = 'Good Afternoon!';
-	    else if (hrs >= 17 && hrs <= 24)
-	        greeting = 'Good Evening!';
+    if (hrs < 12)
+        greeting = 'Good Morning!';
+    else if (hrs >= 12 && hrs <= 17)
+        greeting = 'Good Afternoon!';
+    else if (hrs >= 17 && hrs <= 24)
+        greeting = 'Good Evening!';
 
-	document.getElementById('greet').innerHTML =
-	        greeting;
+    document.getElementById('greet').innerHTML = greeting;
+    document.getElementsByClass('searchbox')[0].focus().click();
 }
